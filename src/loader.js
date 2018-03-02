43cfe5efc93fa42e12528aa6a191161bd4fc27b8
xaresys <jeremy.e.wharton@gmail.com>
2018-03-02 02:32:49 -0600

    Update loader.js

--- src/loader.js
+++ src/loader.js
@@ -1,30 +1,12 @@
 var Loader = pc.createScript('loader');
 
 Loader.prototype.initialize = function() {
-  var url = "https://raw.githack.com/xaresys/PlayCanvas-QuickEngine/loadertest/src/test.js";
+  var url = "https://playcanvas.com/api/assets/files/scripts/fileLoader.js?id=11309558";
   var root = this.entity;
-  
-  /*
-  context.assets.loadFromUrl(url, "script").then(function(result) {
-      var thisjs = result.resource[0];
-      this.entity.model.material.diffuseMap = image;
-      this.entity.model.material.update();
-  }.bind(this), function(err) {
-      console.error(err);
-  });
-  */
   
   this.app.assets.loadFromUrl(url, 'script', function(err, asset) {
     if (err) {console.error(err);return;}
-    
-    /*
-    cubeModel.addComponent("model", {
-        type: "mesh",
-        asset: asset
-    });
-    */
-    
-    root.addComponent('script');
-    root.script.create('test');
+    root.script.create('fileLoader');
+    root.script.move('fileLoader', 0);
   });
 };
