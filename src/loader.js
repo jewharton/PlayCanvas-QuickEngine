25b8775c91acd951701978abe1cd0033c1f94dc6
xaresys <jeremy.e.wharton@gmail.com>
2018-03-01 21:48:01 -0600

    Create loader.js

---
+++ src/loader.js
@@ -0,0 +1,30 @@
+var Loader = pc.createScript('loader');
+
+Loader.prototype.initialize = function() {
+  var url = "https://raw.githubusercontent.com/xaresys/PlayCanvas-QuickEngine/master/src/test.js";
+  var root = this.entity;
+  
+  /*
+  context.assets.loadFromUrl(url, "script").then(function(result) {
+      var thisjs = result.resource[0];
+      this.entity.model.material.diffuseMap = image;
+      this.entity.model.material.update();
+  }.bind(this), function(err) {
+      console.error(err);
+  });
+  */
+  
+  this.app.assets.loadFromUrl(url, 'script', function(err, asset) {
+    if (err) {console.error(err);return;}
+    
+    /*
+    cubeModel.addComponent("model", {
+        type: "mesh",
+        asset: asset
+    });
+    */
+    
+    root.addComponent('script');
+    root.script.create('test');
+  });
+};
